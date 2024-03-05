9d27ff5fbe2dd335901829b67d49b77849281a95
eaglses <danielgay910@yahoo.com>
2024-03-04 16:43:43 -0900

    save for mobile work

---
+++ api/queries/group_queries.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from queries.pool import pool
+from .user_queries import BasicUserOut
+from fastapi import HTTPException
+from datetime import datetime
+
+class Member(BaseModel)
---
+++ api/routers/group_routers.py
@@ -0,0 +1,5 @@
+from fastapi import (
+    Depends,
+    APIRouter,
+)
+from authenticator import authenticator
