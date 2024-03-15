1b59944e82537b1baba3d9457f8c2ebc233dbaba
eaglses <danielgay910@yahoo.com>
2024-03-14 17:58:01 -0800

    innit group members API

--- api/routers/member_routers.py
+++ api/routers/member_routers.py
@@ -14,11 +14,11 @@ from queries.member_queries import (
 router = APIRouter(tags=["members"])
 
 
-@router.post("/api/events/{event_id}/attendees", response_model=Attendee)
+@router.post("/api/group/{group_id}/member", response_model=Attendee)
 def create_attendee(
-    event_id: int,
+    group_id: int,
     repo: AttendeesRepo = Depends(),
     account_data: dict = Depends(authenticator.get_current_account_data),
 ):
     user_id = account_data["id"]
-    return repo.create(event_id, user_id)
+    return repo.create(group_id, user_id)
